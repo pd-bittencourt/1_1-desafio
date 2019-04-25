a21d5be3c42b92b7335e327b74686e3390e7de60
Pedro <pedrodmbc@gmail.com>
2019-04-25 08:56:11 -0300

    /check route

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const app = express()
 
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  return res.render('home')
+})
+
+app.post('/check', (req, res) => {
+  console.log(req.body)
+  if (req.body.age > 18) {
+    return res.redirect('/major')
+  } else {
+    return res.redirect('/minor')
+  }
 })
 
 app.listen(3000)
